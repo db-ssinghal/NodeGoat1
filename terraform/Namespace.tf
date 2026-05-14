d268f182634ac93a608f4becceb30149a48d2be9
HuynhKhoi2711 <huynhanhkhoi9a7@gmail.com>
2026-05-14 22:58:21 +0700

    refactor(terraform): clean up unused resources and update namespace configuration

--- terraform/Namespace.tf
+++ terraform/Namespace.tf
@@ -1,7 +1,7 @@
 # Create a Kubernetes namespace for the NodeGoat staging environment
 resource "kubernetes_namespace" "nodegoat_staging" {
   metadata {
-    name = var.namespace
+    name = "nodegoat-staging"
     labels ={
         environment = "staging"
         app         = "nodegoat"
